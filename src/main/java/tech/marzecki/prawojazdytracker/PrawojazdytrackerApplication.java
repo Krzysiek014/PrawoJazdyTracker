2d4b108686cb714dbdbfbc62e79e323ba177692e
Krzysiek014 <krzysiek014@o2.pl>
2020-09-13 17:33:13 +0200

    Initial commit

---
+++ src/main/java/tech/marzecki/prawojazdytracker/PrawojazdytrackerApplication.java
@@ -0,0 +1,13 @@
+package tech.marzecki.prawojazdytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrawojazdytrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrawojazdytrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/marzecki/prawojazdytracker/PrawojazdytrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.marzecki.prawojazdytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrawojazdytrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

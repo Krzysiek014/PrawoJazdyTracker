a70023ffed6c21c83d21ee62b15101603475dac1
Krzysiek014 <krzysiek014@o2.pl>
2020-12-23 10:28:07 +0100

    Add -> New background image when logout

--- src/main/java/tech/marzecki/prawojazdytracker/mvc/MvcConfig.java
+++ src/main/java/tech/marzecki/prawojazdytracker/mvc/MvcConfig.java
@@ -12,9 +12,9 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         System.out.println("SAF");
-        registry.addResourceHandler("/static/**").addResourceLocations("file:/home/krzysztofm/PROJEKTY/PRAWOJAZDY/PrawoJazdyTracker/src/main/website/build/static/");
-        registry.addResourceHandler("/*.json").addResourceLocations("file:/home/krzysztofm/PROJEKTY/PRAWOJAZDY/PrawoJazdyTracker/src/main/website/build/");
-        registry.addResourceHandler("/*.ico").addResourceLocations("file:/home/krzysztofm/PROJEKTY/PRAWOJAZDY/PrawoJazdyTracker/src/main/website/build/static/");
-        registry.addResourceHandler("/website/lesson/**/index*", "/website/settings/index*", "/website/login/index*", "/website/register/index*", "/website/home/index*").addResourceLocations("file:/home/krzysztofm/PROJEKTY/PRAWOJAZDY/PrawoJazdyTracker/src/main/website/build/index.html");
+        registry.addResourceHandler("/static/**").addResourceLocations("file:/home/krzysztof/PROJEKTY/PrawoJazdyTracker/src/main/website/build/static/");
+        registry.addResourceHandler("/*.json").addResourceLocations("file:/home/krzysztof/PROJEKTY/PrawoJazdyTracker/src/main/website/build/");
+        registry.addResourceHandler("/*.ico").addResourceLocations("file:/home/krzysztof/PROJEKTY/PrawoJazdyTracker/src/main/website/build/static/");
+        registry.addResourceHandler("/website/lesson/**/index*", "/website/settings/index*", "/website/login/index*", "/website/register/index*", "/website/home/index*").addResourceLocations("file:/home/krzysztof/PROJEKTY/PrawoJazdyTracker/src/main/website/build/index.html");
     }
 }
--- src/main/website/src/Login.js
+++ src/main/website/src/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import loginBG from './login.jpg';
+import logoutBG from './logout.jpg'
 import NavigationBar from './components/NavigationBar';
 import './App.css';
 import LoginContainer from './components/LoginContainer';
@@ -10,7 +11,7 @@ function Login() {
     <div >
       <NavigationBar firstTime = {true}>
       </NavigationBar>
-      <img src={loginBG} style={{height: "-webkit-fill-available"}}></img>
+      <img src={window.location.search=='?logout' ? logoutBG : loginBG} style={{height: "-webkit-fill-available"}}></img>
       <LoginContainer></LoginContainer>
     </div>
   );
